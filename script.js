826d245d831c5d5b019cf3ac6d369f933004d5f9
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-11-26 13:13:10 +0000

    Finished basic code

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = "";
 
+
   episodeList.forEach(episode => {
     const card = document.createElement("div");
-    card.textContent = `Name: ${episode.name}`;
+    card.id = "card";
+
+    const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.season).padStart(2, "0")}`;
+
+    card.innerHTML = `<h3>${episode.name} - ${episodeCode}</h3>
+    <img src="${episode.image.medium}" alt="${episode.name}">
+    <p>${episode.summary}</p>
+    `;
 
 
 
