f4d7771734190a70e4200d5e9e4f468137ec29c1
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-12-19 04:11:42 +0000

    completed level 500

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let showContainer = {};
+let allEpisodes		= {};
+let allShows		= {};
+let currentShowId	= 0;
+
+const currentDisplayingElm	= document.getElementById("currentDisplaying");
+const showsSelectElm		= document.getElementById("showsSelect");
+const episodesSelectElm		= document.getElementById("episodesSelect");
+const searchInputElm		= document.getElementById("search-input");
+const rootElem				= document.getElementById("root");
+const backToShowsBtnElm		= document.getElementById("backToShowsBtn");
+const errorMessageElm		= document.getElementById("errorMessage");
+const pageLoadingElm		= document.getElementById("pageLoading");
 
 async function setup() {
-  let allShows = await getShows();
+  allShows = await getShows();
   allShows.sort((a, b) => {
   const nameA = a.name.toUpperCase(); 
   const nameB = b.name.toUpperCase();
@@ -17,80 +28,159 @@ async function setup() {
     createShowOption(show);
   }
 
-  let allEpisodes = await getData(allShows[0].id); // Default | for the first load
-  makePageForEpisodes(allEpisodes);
-  const showsSelect = document.getElementById("showsSelect");
-  const episodesSelect      = document.getElementById("episodesSelect");
-  const searchInput = document.getElementById("search-input");
+  makePageForShows(allShows);
 
-  searchInput.addEventListener("input", () => {
-    const searchWord = searchInput.value.toLowerCase();
-    searchEpisodes(allEpisodes, searchWord);
+  searchInputElm.addEventListener("input", () => {
+    const searchWord = searchInputElm.value.toLowerCase();
+    // if show chosen, search in episodes, if not search in shows
+    if(!allEpisodes || Object.keys(allEpisodes).length === 0){
+      searchShows(allShows, searchWord);
+    } else {
+      searchEpisodes(allEpisodes, searchWord);
+    }
   });
 
-  for( const episode of allEpisodes){
-    createOption(episode);
-  }
+  showsSelectElm.addEventListener("change", async () =>{
+    currentShowId = showsSelectElm.value;
+    pageLoadingElm.textContent = "Loading ...";
 
-  showsSelect.addEventListener("change", async () =>{
-    const valueId = showsSelect.value;
-    document.getElementById("Loading").textContent = "Loading ...";
-    if (valueId === "all") {
-      makePageForEpisodes([]); // clear page
-
-      document.getElementById("episodesSelect").disabled=true;
-      document.getElementById("search-input").disabled=true;
-      document.getElementById("errorMessage").innerHTML =
-       `<div id="errorMessage">Please select a Show to display episodes</div>`;
-
+     
+    if (currentShowId === "all") {
+        makePageForShows(allShows);
+        backToShowsBtnElm.style.display = "none";
+        searchInputElm.disabled=false;
+        episodesSelectElm.disabled=true;
+        episodesSelectElm.innerHTML = `<option value="all">--Select all--</option>`;
       return false;
     }
 
-    document.getElementById("episodesSelect").disabled=false;
-    document.getElementById("search-input").disabled=false;
-    document.getElementById("errorMessage").innerHTML = "";
+    backToShowsBtnElm.style.display = "inline-block";
+    episodesSelectElm.disabled=true;
+    searchInputElm.disabled=false;
+    errorMessageElm.innerHTML = "";
 
-    if (Object.hasOwn(showContainer, valueId)){
-    allEpisodes = showContainer[valueId];
-    }
-    else{
-      allEpisodes = await getData(valueId);
+    allEpisodes = await getData(currentShowId);
 
-      document.getElementById("episodesSelect").innerHTML 
-      = `<option value="all">--Select all--</option>`;
-      for( const episode of allEpisodes){
-        createOption(episode);
-      }
-    }
-    makePageForEpisodes(allEpisodes);
-  })
-  
-
-  episodesSelect.addEventListener("change", async () => {
-    const episodeId = episodesSelect.value;
-
-    document.getElementById("Loading").textContent = "Loading ...";
-    
-    if (episodeId == "all"){
-      makePageForEpisodes(allEpisodes); 
-    }
-    else {
-      const selectedEpisode = [];
-      for (const episode of allEpisodes){
-        if (episode.id == episodeId){
-          selectedEpisode.push(episode);
-        }
-      }
-      makePageForEpisodes(selectedEpisode); 
-    }
+    makePageForEpisodes(currentShowId);
   });
   
+  episodesSelectElm.addEventListener("change", async () => {
+    const episodeId = episodesSelectElm.value;
+
+    pageLoadingElm.textContent = "Loading ...";
+    
+    if (episodeId == "all"){
+      makePageForEpisodes(currentShowId); 
+    }
+    else {
+        const selectedEpisode = [];
+        for (const episode of allEpisodes){
+            if (episode.id == episodeId){
+            selectedEpisode.push(episode);
+            }
+        }
+      displayEpisode(selectedEpisode); 
+    }
+  });
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+function displayEpisode (selectedEpisode) {
+    backToShowsBtnElm.style.display = "inline-block";
+    rootElem.innerHTML = "";
+    pageLoadingElm.innerHTML = "";
+
+  selectedEpisode.forEach(episode => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const episodeName = episode.name ?? "Name Unavailable";
+    let   episodeMediumImage = "img/placeholder.png";
+    const episodeSummary = episode.summary ?? "No Summary Available";
+    const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.season).padStart(2, "0")}`;
+
+    if (episode.image && episode.image.medium)
+      episodeMediumImage = episode.image.medium;
+
+    card.innerHTML = `<h3>${episodeName} - ${episodeCode}</h3>
+    <img src="${episodeMediumImage}" alt="${episodeName}">
+    <p>${episodeSummary}</p>
+    `;
+    rootElem.appendChild(card);
+  });
+}
+
+function displayShow (selectedShow) {
+    backToShowsBtnElm.style.display = "inline-block";
+    rootElem.innerHTML = "";
+    pageLoadingElm.innerHTML = "";
+
+  selectedShow.forEach(show => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const showName = show.name ?? "Name Unavailable";
+    let   showMediumImage = "img/placeholder.png";
+    const showSummary = show.summary ?? "No Summary Available";
+    const showCode = `S${String(show.season).padStart(2, "0")}E${String(show.season).padStart(2, "0")}`;
+
+    if (show.image && show.image.medium)
+      showMediumImage = show.image.medium;
+
+    card.innerHTML = `<h3>${showName} - ${showCode}</h3>
+    <img src="${showMediumImage}" alt="${showName}">
+    <p>${showSummary}</p>
+	<p>Genres: ${show.genres}</p>
+    <p>Status: ${show.status}</p>
+    <p>Rating: ${show.rating.average}</p>
+    <p>Runtime: ${show.runtime}</p>
+    `;
+    rootElem.appendChild(card);
+  });
+}
+
+
+function makePageForShows(allShows) {
   rootElem.innerHTML = "";
-  document.getElementById("Loading").innerHTML = "";
+  pageLoadingElm.innerHTML = "";
+  currentDisplayingElm.textContent = "Displaying " + allShows.length + "/" + allShows.length + " Shows"
+  
+  allShows.forEach(show => {
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+
+    card.innerHTML = `<h3>${show.name}</h3>
+    <img src="${show.image.medium}" alt="${show.name}">
+    <p>${show.summary}</p>
+    <p>Genres: ${show.genres}</p>
+    <p>Status: ${show.status}</p>
+    <p>Rating: ${show.rating.average}</p>
+    <p>Runtime: ${show.runtime}</p>
+    `;
+
+    // add click listener
+    card.addEventListener("click", () => {
+      makePageForEpisodes(show.id);
+    });
+
+    rootElem.appendChild(card);
+  });
+}
+
+async function makePageForEpisodes(showId) {
+  let episodeList = await getData(showId);
+
+  currentDisplayingElm.textContent = "Displaying " + episodeList.length + "/" + episodeList.length + " Episodes"
+  rootElem.innerHTML = "";
+  pageLoadingElm.innerHTML = "";
+  backToShowsBtnElm.style.display = "inline-block";
+  episodesSelectElm.disabled = false;
+  episodesSelectElm.innerHTML = `<option value="all">--Select all--</option>`;
+  
+  for( const episode of episodeList){
+    createOption(episode);
+  }
 
   episodeList.forEach(episode => {
     const card = document.createElement("div");
@@ -111,6 +201,10 @@ function makePageForEpisodes(episodeList) {
     <p>${episodeSummary}</p>
     `;
     rootElem.appendChild(card);
+
+    card.addEventListener("click", () => {
+        displayEpisode([episode]);
+    });
   });
 }
 
@@ -121,23 +215,31 @@ function searchEpisodes(allEpisodes, searchWord){
       result.push(episode);
     }
   }
-  makePageForEpisodes(result);
+  displayEpisode(result);
   
-  const currentDisplaying = document.getElementById("currentDisplaying");
-  currentDisplaying.textContent = "Displaying " + result.length + "/" + allEpisodes.length + " Episodes";
+  currentDisplayingElm.textContent = "Displaying " + result.length + "/" + allEpisodes.length + " Episodes";
 
-  if (searchWord.length == 0) {
-    currentDisplaying.innerHTML = "";
+}
+
+function searchShows(allShows, searchWord){
+  const result = [];
+  for ( const show of allShows){
+    if (show.name.toLowerCase().includes(searchWord) || show.summary.toLowerCase().includes(searchWord)){
+      result.push(show);
+    }
   }
+  displayShow(result);
+  
+  currentDisplayingElm.textContent = "Displaying " + result.length + "/" + allShows.length + " Shows";
+
 }
 
 function createOption(episode){
   const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
-  const option = document.createElement("option")
+  const option = document.createElement("option");
   option.textContent = episodeCode + " - "+ episode.name;
   option.value = episode.id;
-  const select = document.getElementById("episodesSelect");
-  select.appendChild(option);
+  episodesSelectElm.appendChild(option);
 }
 
 function createShowOption(show){
@@ -148,7 +250,7 @@ function createShowOption(show){
   select.appendChild(option);
 }
 
-async function getData(id) {
+async function getData(id){
   const url = `https://api.tvmaze.com/shows/${id}/episodes`;
   try {
     const response = await fetch(url);
