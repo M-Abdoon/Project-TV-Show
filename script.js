c1b0ab8fa854228c29d10ad710d78f50b5372b94
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-07-10 17:07:15 +0100

    changed condition

--- script.js
+++ script.js
@@ -126,7 +126,13 @@ function displayShow(selectedShow) {
 
 		const showName = show.name ?? "Name Unavailable";
 		let showMediumImage = "img/placeholder.png";
-		const showSummary = show.summary ?? "No Summary Available";
+		//const showSummary = show.summary ?? "No Summary Available";
+		const showSummary = "";
+
+		if (!show.summary)
+			showSummary = "No Summary Available";
+		else
+			showSummary = show.summary;
 
 		if (show.image && show.image.medium)
 			showMediumImage = show.image.medium;
